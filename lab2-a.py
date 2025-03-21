7755e333ea352f5f4e97ff8035c191447f9a5c1c
ihebda <ihebda@ces.edu.pl>
2025-03-21 13:35:49 +0100

    agents2

--- lab2-a.py
+++ lab2-a.py
@@ -2,21 +2,26 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
-#from agents import Agent, Runner
+from agents import Agent, Runner
 import agents
 
 import asyncio 
 import nest_asyncio
 nest_asyncio.apply()
 
-# Load environment variables
-dotenv_path = os.path.join('../', 'config', '.env')
-load_dotenv(dotenv_path)
-api_key = os.getenv('OPENAI_API_KEY_TEG')
-os.environ["OPENAI_API_KEY"] = api_key  
-agents.set_default_openai_key(api_key, True)
 
+async def main():
+    # Load environment variables
+    dotenv_path = os.path.join('../', 'config', '.env')
+    load_dotenv(dotenv_path)
+    api_key = os.getenv('OPENAI_API_KEY_TEG')
+    os.environ["OPENAI_API_KEY"] = api_key  
+    agents.set_default_openai_key(api_key, True)
+    agent = Agent(name="Assistant", instructions="You are a helpful assistant")
+    #result = await agents.Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+    result = Runner.run_sync(agent, "Write a haiku about recursion in programming.")
+    result = await Runner.run(agent, "Write a haiku about recursion in programming.")
+    print(result.final_output)
 
-agent = agents.Agent(name="Assistant", instructions="You are a helpful assistant")
-result = await agents.Runner.run_sync(agent, "Write a haiku about recursion in programming.")
-print(result.final_output)
+if __name__ == "__main__":
+    asyncio.run(main())
